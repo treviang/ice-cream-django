0fbe0bbc32f04cf2126d6ebb4e57c449ed7f3e83
Angelo Trevisiol <angelo.trevisiol@gmail.com>
2023-12-29 00:47:36 +0100

    Creata applicazione ricette

---
+++ ricette/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from djmoney.models.fields import MoneyField
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
+
+class Ingrediente(models.Model):
+    codice = models.CharField(max_length=20)
+    descrizione = models.CharField(max_length=200)
+    zuccheri = models.DecimalField(max_digits=3, decimal_places=2, validators=PERCENTAGE_VALIDATOR)
+    grassi = models.DecimalField(max_digits=3, decimal_places=2, validators=PERCENTAGE_VALIDATOR)
+    SIng = models.DecimalField(max_digits=3, decimal_places=2, validators=PERCENTAGE_VALIDATOR)
+    altri_solidi = models.DecimalField(max_digits=3, decimal_places=2, validators=PERCENTAGE_VALIDATOR)
+    acqua = models.DecimalField(max_digits=3, decimal_places=2, validators=PERCENTAGE_VALIDATOR)
+    PAC = models.DecimalField(max_digits=4, decimal_places=2)
+    POD = models.DecimalField(max_digits=4, decimal_places=2)
+    costo = MoneyField(max_digits=14, decimal_places=2, default_currency='EUR')
+    valore_energetico = models.DecimalField(max_digits=4, decimal_places=2)
+    grassi = models.DecimalField(max_digits=4, decimal_places=2)
+    grassi_saturi = models.DecimalField(max_digits=4, decimal_places=2)
+    carboidrati = models.DecimalField(max_digits=4, decimal_places=2)
+    carboidrati_zuccheri = models.DecimalField(max_digits=4, decimal_places=2)
+    proteine = models.DecimalField(max_digits=3, decimal_places=2)
+    sale = models.DecimalField(max_digits=3, decimal_places=3)
