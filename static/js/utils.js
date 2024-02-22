796bacd8ecbc5ac040ef6c3395f0c54a2677a79c
Angelo Trevisiol <angelo.trevisiol@gmail.com>
2024-02-22 23:25:02 +0100

    recupero costo dinamicamente

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     let miscelaForm = document.querySelectorAll(".miscela-form")
-    let container = document.getElementById("form-container")
     let addIngrediente = document.querySelector("#add-ingrediente")
     let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
+    let tabella = document.getElementById('ingredienti-table');
 
-    
+
     let formNum = miscelaForm.length - 1
     addIngrediente.addEventListener('click', addForm)
-    
+
     function addForm(e) {
         e.preventDefault()
-        
+
         let newForm = miscelaForm[0].cloneNode(true)
         let formRegex = RegExp(`form-(\\d){1}-`, 'g')
-        
+
         formNum++
         newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
-        container.insertBefore(newForm, addIngrediente)
-        
+        tabella.appendChild(newForm)
+
         totalForms.setAttribute('value', `${formNum + 1}`)
     }
 
     let selectIngrediente = document.querySelector('.sel-ingrediente');
     selectIngrediente.addEventListener('change', showOnChange);
 
-    function showOnChange(e){
+    function showOnChange(e) {
         var selectedOption = e.target.value;
 
         let options = {
@@ -33,11 +33,10 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         fetch('/get_ingrediente/' + selectedOption, options)
-        .then(response => response.json())
-        .then(body => {
-                console.log(body)
-                // Do something with body
-        });
+            .then(response => response.json())
+            .then(body => {
+                document.getElementById('costo').innerHTML = body[0].costo;
+            });
 
     }
 })
